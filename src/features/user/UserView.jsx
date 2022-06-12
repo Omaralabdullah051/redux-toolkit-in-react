a6bb72068a73fe9efa90e57216a86e396d335629
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-12 08:51:41 +0400

    fetching data with redux toolkit in react

--- src/features/user/UserView.jsx
+++ src/features/user/UserView.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from './userSlice';
 
 const UserView = () => {
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.user);
+    useEffect(() => {
+      dispatch(fetchUsers());
+    },[]);
     return (
         <div>
             <h2>List of Users</h2>
+            {user.loading && <p>Loading...</p>}
+            {!user.loading && user.error ? <p>{user.error}</p> : null}
+            {!user.loading && user.users.length ? (
+                <ul>
+                    {user.users.map(user => (
+                        <li key={user.id}>{user.name}</li>
+                    ))}
+                </ul>
+            ) : null}
         </div>
     );
 };
